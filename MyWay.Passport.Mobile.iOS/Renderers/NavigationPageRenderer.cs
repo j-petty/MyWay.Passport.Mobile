10738ce76072b2ef8287b19148ae53b7008f1b27
James Petty <j-petty@outlook.com>
2021-04-19 19:32:15 +1000

    Disabled iOS NavigationBar shadow

---
+++ MyWay.Passport.Mobile.iOS/Renderers/NavigationPageRenderer.cs
@@ -0,0 +1,19 @@
+﻿using MyWay.Passport.Mobile.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(NavigationPage), typeof(NavigationPageRenderer))]
+namespace MyWay.Passport.Mobile.iOS.Renderers
+{
+    public class NavigationPageRenderer : NavigationRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            // Disable NavigationBard shadow
+            NavigationBar.ShadowImage = new UIImage();
+        }
+    }
+}

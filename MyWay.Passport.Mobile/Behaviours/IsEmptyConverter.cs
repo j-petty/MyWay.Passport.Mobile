bcb74aa3d60f41ed8996fc998f5bcf5d6395602a
James Petty <j-petty@outlook.com>
2021-04-30 10:43:30 +1000

    fixed message for empty list

--- MyWay.Passport.Mobile/Behaviours/IsEmptyConverter.cs
+++ MyWay.Passport.Mobile/Behaviours/IsEmptyConverter.cs
@@ -1,17 +1,18 @@
 ﻿using System;
+using System.Collections;
 using System.Globalization;
 using Xamarin.Forms;
 
 namespace MyWay.Passport.Mobile.Behaviours
 {
     /// <summary>
-    /// Returns true if the value is null
+    /// Returns true if the value is an empty list
     /// </summary>
-    public class IsNullConverter : IValueConverter
+    public class IsEmptyConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value == null;
+            return value == null || ((IList)value)?.Count == 0;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)

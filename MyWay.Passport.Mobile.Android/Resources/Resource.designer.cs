e34cdda8dc2b19c505be0c5dcf26f294889cbb0a
James Petty <j-petty@outlook.com>
2021-05-02 23:26:27 +1000

    removed redundant icons

--- MyWay.Passport.Mobile.Android/Resources/Resource.designer.cs
+++ MyWay.Passport.Mobile.Android/Resources/Resource.designer.cs
@@ -13254,12 +13254,6 @@ namespace MyWay.Passport.Mobile.Droid
 			// aapt resource value: 0x7F0C0000
 			public const int icon = 2131492864;
 			
-			// aapt resource value: 0x7F0C0001
-			public const int icon_round = 2131492865;
-			
-			// aapt resource value: 0x7F0C0002
-			public const int launcher_foreground = 2131492866;
-			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
